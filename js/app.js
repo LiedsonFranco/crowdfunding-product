6a255a1815c27aee7701649a1097d7a97a5f3de4
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-05-18 13:08:55 +0100

    Design completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+let porcente = 0
+let total_backed = 0
+let total_backers = 0
+let is_menu_open = false
+let objective_money = 100000
 let inputs = document.querySelectorAll('.input_radio')
-let last_hidden_section 
-let total_backed = 89.914
+let porcente_tag = document.getElementById('progress-bar')
 let total_backed_tag = document.getElementById('total_backed')
+let total_backers_tag = document.getElementById('total_backers')
+let projects = {'Bamboo Stand': [101, document.querySelectorAll('#bamboo_stand')],'Black Edition Stand':[64, document.querySelectorAll('#black_edition_stand')]}
+let last_hidden_section 
 
 inputs.forEach(element => {
     element.addEventListener('change', () => {
@@ -16,13 +23,39 @@ inputs.forEach(element => {
 function close_modal(id,button){
     document.getElementById(id).classList.add('hidden')
     if(id == 'form_modal'){
-        total_backed +=  parseFloat(document.getElementById(button.dataset.get).value)
+        let input_tag = document.getElementById(button.dataset.get)
+        projects[button.dataset.project][0] -= 1 
+        projects[button.dataset.project][1].forEach(element =>  {
+            element.innerHTML = projects[button.dataset.project][0]
+        })
+        total_backed +=  parseFloat(input_tag.value)
         total_backed_tag.innerText = total_backed
+
+        porcente = parseInt((total_backed * 100) / objective_money)
+        porcente_tag.style.width = `${porcente}%`
+
+        total_backers += 1
+        total_backers_tag.innerHTML = total_backers
+       
     }
 }
 function show_modal(id){
     document.getElementById(id).classList.remove('hidden')
 }
-function add_to_backed(){
-    
+
+function show_menu(){
+    if(is_menu_open){
+        is_menu_open = false
+        document.getElementById('menu').classList.add('hidden')
+        document.querySelector('menu').classList.remove('cover_shadow')
+
+        document.getElementById('button_img').src = 'images/icon-hamburger.svg'
+    }
+    else{
+        is_menu_open = true
+        document.getElementById('menu').classList.remove('hidden')
+        document.getElementById('menu').classList.add('cover_shadow')
+
+        document.getElementById('button_img').src = 'images/icon-close-menu.svg'
+    }
 }
